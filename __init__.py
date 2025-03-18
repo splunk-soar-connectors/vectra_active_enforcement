8f79057bedb7312ca702dd6a3c173dbcd10c98a5
Ian Rokas <grokas@splunk.com>
2025-03-18 08:14:33 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # File: __init__.py
 #
-# Copyright Vectra 2017-2023
+# Copyright Vectra 2017-2025
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
